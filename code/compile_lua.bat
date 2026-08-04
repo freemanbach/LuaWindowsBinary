5007e0e8f8a0c5400816d5d1cbf3e6431a1dd3db
Freemanbach <flo@radford.edu>
2026-08-04 11:54:23 -0400

    made some major changes

--- code/compile_lua.bat
+++ code/compile_lua.bat
@@ -13,28 +13,38 @@
 :: Start local variable scope
 setlocal
 
-set vsdevcmd="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-set vsdev2026="C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
+set vsdev22="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+set vsdev26="C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
 
 :: Start the search and variable execution Process
-:start_me
-  if not exist %vsdevcmd% (
-    echo. There is no Visual Studio 2022 Community edition installed.
-    echo. Please first Download and install VS-2022 Community edition.
-    echo. https://visualstudio.microsoft.com/vs/compare/
-    goto end
+:check_vs26
+  if exist %vsdev26% (
+    call %vsdev26%
+  ) else (
+        echo.
+        echo. There is no Visual Studio 2026 Community edition installed.
+        echo. Please first Download and install VS-2026 Community edition.
+        echo. https://visualstudio.microsoft.com/vs/compare/
+      goto check_vs22
+  )
+
+:check_vs22 
+  if exist %vsdev22% (
+    call %vsdev22%
     ) else (
-      echo. Executing VsDevCmd.bat variables
-      call %vsdevcmd%
-      echo.
-      echo.
+        echo.
+        echo. There is no Visual Studio 2022 Community edition installed.
+        echo. Please first Download and install VS-2022 Community edition.
+        echo. https://visualstudio.microsoft.com/vs/compare/
+        goto end
     )
 
-:: Identify the target architecture
-if /i "%PROCESSOR_ARCHITECTURE%"=="x86" set ARCH=x86
-if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" set ARCH=amd64
-if /i "%PROCESSOR_ARCHITECTURE%"=="ARM64" set ARCH=arm64
-if /i "%ARCH%"=="" goto end
+:arch_type
+  :: Identify the target architecture
+  if /i "%PROCESSOR_ARCHITECTURE%"=="x86" set ARCH=x86
+  if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" set ARCH=amd64
+  if /i "%PROCESSOR_ARCHITECTURE%"=="ARM64" set ARCH=arm64
+  if /i "%ARCH%"=="" goto end
 
 ::
 :: Process files
@@ -42,78 +52,103 @@ if /i "%ARCH%"=="" goto end
 :: the Below portion was copied from here:
 :: https://github.com/Pharap/CompilingLua/blob/master/Compile.bat
 
+: handle_file
+  echo.
+  echo. ============================
+  echo.  Making directory
+  echo. ============================
+  echo.
+  :: Make a Directory for exe and dll files
+  @mkdir bin
 
-:: Make a Directory for exe and dll files
-@mkdir bin
+  :: Move down into 'src'
+  @pushd src
 
-:: Move down into 'src'
-@pushd src
+:compile_it
+  :: This batch file will show details Windows 10
+  echo.
+  echo. ============================
+  echo.  Compiling Object File
+  echo. ============================
+  echo.
+  :: Clean up files from previous builds
+  @if EXIST *.o @del *.o
+  @if EXIST *.obj @del *.obj
+  @if EXIST *.dll @del *.dll
+  @if EXIST *.exe @del *.exe
 
-:: This batch file will show details Windows 10
-echo.
-echo. ============================
-echo. Compiling Object File
-echo. ============================
-echo.
-:: Clean up files from previous builds
-@if EXIST *.o @del *.o
-@if EXIST *.obj @del *.obj
-@if EXIST *.dll @del *.dll
-@if EXIST *.exe @del *.exe
+  :: Compile all .c files into .obj
+  @CL /MD /O2 /c /DLUA_BUILD_AS_DLL *.c
 
-:: Compile all .c files into .obj
-@CL /MD /O2 /c /DLUA_BUILD_AS_DLL *.c
+  :: Rename two special files
+  @ren lua.obj lua.o
+  @ren luac.obj luac.o
 
-:: Rename two special files
-@ren lua.obj lua.o
-@ren luac.obj luac.o
+:finish_compile
+  echo.
+  echo. ===========================
+  echo.  Finished Compiling
+  echo. ===========================
+  echo.
 
-echo. ===========================
-echo. Finished Compiling
-echo. ===========================
+:linking_files
+  echo.
+  echo. ============================
+  echo.  Linking Object Files
+  echo. ============================
+  echo.
+  :: Link up all the other .objs into a .lib and .dll file
+  @LINK /DLL /IMPLIB:lua.lib /OUT:lua.dll *.obj
 
-echo.
-echo. ============================
-echo. Linking Object Files
-echo. ============================
-echo.
-:: Link up all the other .objs into a .lib and .dll file
-@LINK /DLL /IMPLIB:lua.lib /OUT:lua.dll *.obj
+  :: Link lua into an .exe
+  @LINK /OUT:lua.exe lua.o lua.lib
 
-:: Link lua into an .exe
-@LINK /OUT:lua.exe lua.o lua.lib
+  :: Create a static .lib
+  @LIB /OUT:lua-static.lib *.obj
 
-:: Create a static .lib
-@LIB /OUT:lua-static.lib *.obj
+  :: Link luac into an .exe
+  @LINK /OUT:luac.exe luac.o lua-static.lib
 
-:: Link luac into an .exe
-@LINK /OUT:luac.exe luac.o lua-static.lib
+  :: Move back up out of 'src'
+  @popd
 
-:: Move back up out of 'src'
-@popd
+:completed_linking
+  echo.
+  echo. ===============================
+  echo.  Finished Linking Object Files
+  echo. ===============================
+  echo.
 
-echo. ===============================
-echo. Finished Linking Object Files
-echo. ===============================
+:moving_files
+  :: Copy the library and executable files out from 'src'
+  echo.
+  echo. ===============================
+  echo.  Moving some files around
+  echo. ===============================
+  echo.
+  @copy /Y src\lua.exe lua.exe
+  @copy /Y src\luac.exe luac.exe
+  @copy /Y src\lua.dll lua.dll
 
-:: Copy the library and executable files out from 'src'
-@copy /Y src\lua.exe lua.exe
-@copy /Y src\luac.exe luac.exe
-@copy /Y src\lua.dll lua.dll
+:copying_files
+  echo.
+  echo. =============================
+  echo.  Copy binary files to Bin dir
+  echo. =============================
+  echo.
+  @copy /Y lua.exe .\bin
+  @copy /Y luac.exe .\bin
+  @copy /Y lua.dll .\bin
 
+:wrapping_up
+  :: Deleting the files in src
+  @del /q /f src\*.exe
+  @del /q /f *.exe
+  @del /q /f src\*.dll
+  @del /q /f *.dll
 
-echo. =============================
-echo. Copy binary files to Bin dir
-echo. =============================
-@copy /Y lua.exe .\bin
-@copy /Y luac.exe .\bin
-@copy /Y lua.dll .\bin
-
-:: Deleting the files in src
-@del /q /f src\*.exe
-@del /q /f *.exe
-@del /q /f src\*.dll
-@del /q /f *.dll
-
+:done
+  echo.
+  echo. Finished.
 :: End local variable scope
 :end
